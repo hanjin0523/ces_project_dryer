2136acd235d36750b3bb51fe593bf8e288afca20
sanchezJang <anjdi1004@gmail.com>
2023-11-17 15:54:08 +0900

    11/17구조체작업중

--- backend/packet.py
+++ backend/packet.py
@@ -1,11 +1,38 @@
 from dataclasses import dataclass
 
 @dataclass
-class default_packet:
+class Base_packet:
     sender: int
     size: int
     p_type: int
     cmd_type: int
+
+    # def base_packet(self):
+        
+    #     packet = sender_bytes + size_bytes + p_type_bytes + res_type_bytes
+    #     return packet
+
+@dataclass
+class Id_reponse_packet(Base_packet):
+    device_id: int
+    result: int
+    etx: bytes = b'\x0D\x0A'
+
+    def create_packet(self):
+        sender_bytes = bytes([self.sender])
+        size_bytes = bytes([self.size])
+        p_type_bytes = bytes([self.p_type])
+        res_type_bytes = bytes([self.cmd_type])
+        device_id_bytes = self.device_id
+        result_bytes = bytes([self.result])
+        etx_bytes = self.etx
+        packet = sender_bytes + size_bytes + p_type_bytes + res_type_bytes + device_id_bytes + result_bytes + etx_bytes
+        print(packet,"id응답패킷생성!!!")
+        return packet
+
+@dataclass
+class Default_packet(Base_packet):##세션응답, 센서정보요청, 에러체크, 동작정지, 일지정지,
+
     device_id: int
     max_packet: int
     current_packet: int
@@ -13,17 +40,40 @@ class default_packet:
     etx: bytes = b'\x0D\x0A'
 
     def create_packet(self):
-        self.sender = bytes([self.sender])
-        self.size = bytes([self.size])
-        self.p_type = bytes([self.p_type])
-        self.cmd_type = bytes([self.cmd_type])
-        self.device_id = bytes(self.device_id)
-        self.max_packet = bytes([self.max_packet])
-        self.current_packet = bytes([self.current_packet])
-        self.result = bytes([self.result])
-        self.etx = bytes(self.etx)
-        packet = self.sender + self.size + self.p_type + self.cmd_type + self.device_id + self.max_packet + self.current_packet + self.result + self.etx
+        sender_bytes = bytes([self.sender])
+        size_bytes = bytes([self.size])
+        p_type_bytes = bytes([self.p_type])
+        res_type_bytes = bytes([self.cmd_type])
+        device_id_bytes = self.device_id
+        max_packet_bytes = bytes([self.max_packet])
+        current_packet_bytes = bytes([self.current_packet])
+        result_bytes = bytes([self.result])
+        etx_bytes = bytes(self.etx)
+        packet = sender_bytes + size_bytes + p_type_bytes + res_type_bytes + device_id_bytes + max_packet_bytes + current_packet_bytes + result_bytes + etx_bytes
+        print(packet,"Default_packet!!!")
         return packet
 
-data = default_packet(0, 15, 2, 2, [23,10,23,0,0,1], 1, 1, 0)
-print(data.create_packet())
+@dataclass
+class Default_packet1(Base_packet):##세션응답, 센서정보요청, 에러체크, 동작정지, 일지정지,
+
+    device_id: int
+    max_packet: int
+    current_packet: int
+    result: int
+    etx: bytes = b'\x0D\x0A'
+
+    def create_packet(self):
+        sender_bytes = bytes([self.sender])
+        size_bytes = bytes([self.size])
+        p_type_bytes = bytes([self.p_type])
+        res_type_bytes = bytes([self.cmd_type])
+        device_id_bytes = self.device_id
+        max_packet_bytes = bytes([self.max_packet])
+        current_packet_bytes = bytes([self.current_packet])
+        result_bytes = bytes([self.result])
+        etx_bytes = bytes(self.etx)
+        packet = sender_bytes + size_bytes + p_type_bytes + res_type_bytes + device_id_bytes + max_packet_bytes + current_packet_bytes + result_bytes + etx_bytes
+        print(packet,"session_response패킷생성!!!")
+        return packet
+
+
